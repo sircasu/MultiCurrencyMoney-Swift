da69ffaa94b7f71375974b3bbe33e087b40dcbcd
Matteo Casu <matteo.casu91@gmail.com>
2024-02-15 20:55:41 +0100

    Initial Commit

---
+++ MultiCurrencyMoney/MultiCurrencyMoney.h
@@ -0,0 +1,18 @@
+//
+//  MultiCurrencyMoney.h
+//  MultiCurrencyMoney
+//
+//  Created by Matteo Casu on 15/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MultiCurrencyMoney.
+FOUNDATION_EXPORT double MultiCurrencyMoneyVersionNumber;
+
+//! Project version string for MultiCurrencyMoney.
+FOUNDATION_EXPORT const unsigned char MultiCurrencyMoneyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiCurrencyMoney/PublicHeader.h>
+
+
